725aa171c0106dc94a19b6de484f6dc9a4679bcb
Danil <heafraer@gmail.com>
2025-11-24 20:14:43 +0300

    Add Hibernate and H2 DB dependencies

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,11 +1,19 @@
 module com.taxtelecom.documentapp {
+    requires javafx.base;
     requires javafx.controls;
     requires javafx.fxml;
     requires transitive javafx.graphics;
-    requires java.sql; // если будет БД
-    requires lombok;
+    requires java.sql;
     
+    requires com.h2database;
 
+    requires org.hibernate.orm.core;
+    requires jakarta.persistence;
+    
+    requires lombok;
+
+    opens com.taxtelecom.model to org.hibernate.orm;
     opens com.taxtelecom to javafx.fxml;
+
     exports com.taxtelecom;
 }
